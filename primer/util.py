19f06927f1ac1d928976620b66546fb4af8567b7
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2021-03-22 19:17:16 +0100

    primer updated

--- primer/util.py
+++ primer/util.py
@@ -1,7 +1,84 @@
+MQL_RESULTS = {
+    1: {
+        "file": "2017_q4439_text_Bas_Meeuse",
+        "results": 8,
+        "verses": 8,
+        "words": 42,
+    },
+    2: {
+        "file": "2017_q4440_text_Bas_Meeuse",
+        "results": 156,
+        "verses": 136,
+        "words": 294,
+    },
+    "2a": {
+        "file": "2017_q4467_text_Dirk_Roorda",
+        "results": 160,
+        "verses": 138,
+        "words": 300,
+    },
+    3: {
+        "file": "2017_q4441_text_Bas_Meeuse",
+        "results": 308,
+        "verses": 150,
+        "words": 685,
+    },
+    4: {
+        "file": "2017_q53_text_Wido_van Peursen",
+        "results": 65,
+        "verses": 51,
+        "words": 315,
+    },
+    5: {
+        "file": "2017_q494_text_Oliver_Glanz",
+        "results": 23,
+        "verses": 21,
+        "words": 44,
+    },
+    6: {
+        "file": "2017_q4442_text_Bas_Meeuse",
+        "results": 10,
+        "verses": 10,
+        "words": 121,
+    },
+    7: {
+        "file": "2017_q4334_text_Reinoud_Oosting",
+        "results": 62,
+        "verses": 61,
+        "words": 348,
+    },
+    8: {
+        "file": "2017_q523_text_Reinoud_Oosting",
+        "results": 16,
+        "verses": 16,
+        "words": 32,
+    },
+    9: {
+        "file": "2017_q491_text_Oliver_Glanz",
+        "results": 344,
+        "verses": 101,
+        "words": 356,
+    },
+    "10a": {
+        "file": "2017_q4416_text_Bas_Meeuse",
+        "results": 232,
+        "verses": 190,
+        "words": 458,
+    },
+    "10b": {
+        "file": "2017_q4437_text_Bas_Meeuse",
+        "results": 2087,
+        "verses": 675,
+        "words": 5573,
+    },
+}
+
+
 def getTfVerses(A, results, focus):
     api = A.api
     L = api.L
     F = api.F
+    T = api.T
 
     verses = set()
     words = set()
@@ -11,10 +88,15 @@ def getTfVerses(A, results, focus):
             focusNode = result[pos]
             if focusNode is None:
                 continue
-            verse = L.u(focusNode, otype="verse")[0]
+            if focusNode == -1:
+                continue
+            focusType = F.otype.v(focusNode)
+            verse = T.sectionTuple(focusNode, fillup=True)[-1]
+            verses.add(verse)
+            verse = T.sectionTuple(focusNode, lastSlot=True, fillup=True)[-1]
             verses.add(verse)
 
-            if F.otype.v(focusNode) == "word":
+            if focusType == "word":
                 words.add(focusNode)
             else:
                 for word in L.d(focusNode, otype="word"):
